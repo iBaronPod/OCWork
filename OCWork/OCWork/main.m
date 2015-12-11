3ad6f3e40e80d07af3704b79afb720babb5ef7bf
李嘉 <achievejia@163.com>
2015-12-11 15:01:17 +0800

    新增初始页面

---
+++ OCWork/OCWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCWork
+//
+//  Created by 李嘉 on 15/12/11.
+//  Copyright © 2015年 李嘉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
